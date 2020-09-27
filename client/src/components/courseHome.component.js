6e39e6df11e45c34dc2366bf4ce3f09429a47061
piblo <paulhankins99@gmail.com>
2020-09-27 10:09:06 -0500

    username in navbar

--- client/src/components/courseHome.component.js
+++ client/src/components/courseHome.component.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Card, CardDeck, Image } from "react-bootstrap";
+import { Button, Card, CardDeck } from "react-bootstrap";
 
 function HookCourseHome() {
   return (
-    
     <CardDeck>
-      <Card style={{ width: "18rem", height: "300px"}}>
+      <Card style={{ width: "18rem", height: "300px" }}>
         <Card.Img
           variant="top"
-          src={require('./constructionCat.png')}
+          src={require("../images/constructionCat.png")}
           alt="Under Construction"
         />
         <Card.Body>
@@ -21,10 +20,10 @@ function HookCourseHome() {
           <Button variant="primary">View</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: "18rem", height: "300px"}}>
+      <Card style={{ width: "18rem", height: "300px" }}>
         <Card.Img
           variant="top"
-          src={require('./constructionCat.png')}
+          src={require("../images/constructionCat.png")}
           alt="Under Construction"
         />
         <Card.Body>
@@ -36,10 +35,10 @@ function HookCourseHome() {
           <Button variant="primary">View</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: "18rem", height: "300px"}}>
+      <Card style={{ width: "18rem", height: "300px" }}>
         <Card.Img
           variant="top"
-          src={require('./constructionCat.png')}
+          src={require("../images/constructionCat.png")}
           alt="Under Construction"
         />
         <Card.Body>
@@ -51,36 +50,7 @@ function HookCourseHome() {
           <Button variant="primary">View</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: "18rem", height: "300px"}}>
-        <Card.Img
-          variant="top"
-          src={require('./constructionCat.png')}
-          alt="Under Construction"
-        />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">View</Button>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: "18rem", height: "300px"}}>
-        <Card.Img
-          variant="top"
-          src={require('./constructionCat.png')}
-          alt="Under Construction"
-        />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">View</Button>
-        </Card.Body>
-      </Card>
+
     </CardDeck>
   );
 }
