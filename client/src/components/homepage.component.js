68e4a0dde0070194c9a00ad9d534e1a041e70ae5
JP <johnpaul.hernandez@selu.edu>
2020-10-06 05:18:52 -0500

    quick error fix i missed

--- client/src/components/homepage.component.js
+++ client/src/components/homepage.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Card} from 'react-bootstrap';
-import "./CSS/homepage.css"
+
 
 
 function HookHome() {
@@ -8,7 +8,7 @@ function HookHome() {
     <div classname="homePage">
       <header className = "Acatemy Header">
       <h1>Welcome to Tabby Acatemy!</h1>
-      <p>The goal of Tabby Acatemy is to help teach important subject fields through the light-heart theme of tabby cats!</p>
+      <p className="MissionStatement">The goal of Tabby Acatemy is to help teach important subject fields through the light-heart theme of tabby cats!</p>
         <Card classname = "card-body" style= {{width: "18rem", height: "200px", align: "center"}}>
           <Card.Img src={require('./cat-cap-gown.png')}/>
         </Card>
