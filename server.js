a6eb4c7fb7b151c542d1a105613f1db7ce1ca189
piblo <paulhankins99@gmail.com>
2020-10-09 14:29:41 -0500

    make it better

--- server.js
+++ server.js
@@ -111,16 +111,17 @@ app.use("/logout", logoutRouter);
 app.use("/auth", require("./routes/Gauth"));
 
 app.get("/", (req, res, next) => {
+  res.redirect('http://localhost:3000')
   
-  if(process.env.NODE_ENV === 'production') {
-    res.redirect('https://guarded-forest-58961.herokuapp.com')
-  } else {
-    res.redirect('http://localhost:3000')
-  }
 });
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
+
+  app.get("/", (req, res, next) => {
+    res.redirect('https://guarded-forest-58961.herokuapp.com')
+    next()
+  });
   
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, "/client", "build", "index.html"))
