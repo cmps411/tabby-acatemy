8484244ff09f84076a0ef856e1bbb7eefb69c1a9
piblo <paulhankins99@gmail.com>
2020-10-10 02:06:46 -0500

    make it better

--- server.js
+++ server.js
@@ -59,7 +59,9 @@ cors: {
 
 app.use(cors(conf.cors));
 
-app.use(function(req, res, next) { // update to match the domain you will make the request from
+app.all('*', function(req, res, next) { 
+  var origin = req.get('origin'); 
+  res.header('Access-Control-Allow-Origin', origin);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET");
