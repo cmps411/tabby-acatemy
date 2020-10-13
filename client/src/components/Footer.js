fad8fbfed6a7eecc04a40483b3307601fce4a8f8
JP <johnpaul.hernandez@selu.edu>
2020-10-13 06:50:15 -0500

    added more content to landing page, still not finished as it still feels empty and boring with text will further work on design once i can get more input for ideas added a contact us mutton in footer that will bring up all teammates emails in a alert, can be further developed to be more professional added a silly address that is just the university's address but with cat puns

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,26 +1,54 @@
 import React from 'react'
+// import {Link} from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 import './CSS/Footer.css'
 
 function Footer(){
     return(
         <div className="main-footer">
             <div className="container">
-            {/* Where we will stick the relevent information to fill out the footer */}
-                <hr />
-                   <div className='row'>
+                <div className='row'>
+                    <div className='col'>
+                        <Button variant='outline-dark' className='ContactUs' onClick={ShowEmails}>
+                            Contact Us!
+                        </Button>
+                    </div>
+
+                    {/* excessive cols because idk a better way to make the space work better lmao */}
+                    <div className='col'></div>
+                    {/* <div className='col'></div> */}
+
+                    <div className='col'>
                         <p>
-                            &copy;{new Date().getFullYear()} Tabby Acatemy | All Meows Reserved | Purrs of Service | No Privacy, Only Cats
+                            Tabby Acatemy Incorpurrated <br/>
+                            500 West Unipurrsity Meowvenue <br/>
+                            Pawmond, LA 70402
                         </p>
-                   </div>
+                    </div>
+                </div>
+            {/* Where we will stick the relevent information to fill out the footer */}
+            <hr />
+                <div className='row'>
+                    <div className='col'>
+                        <div>
+                            <p>
+                                &copy;{new Date().getFullYear()} Tabby Acatemy | All Kittens Reserved | Purrs of Service | Privacy Pawlicy
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
+    function ShowEmails(props){
+        alert("johnpaul.hernandez@selu.edu | paul.hankins@selu.edu | hoan.nguyen@selu.edu | caitlin.farmer@selu.edu | zachary.ostrowksi@selu.edu | patrick.allen@selu.edu")
+    }
 }
 export default Footer;
 /* To be used to fill out the footer with relevent information, but for now it looks too weird
 <div className="row">   
                     
-                    <div className="Column">
+                    <div className="col">
                         <h4>The Team</h4>
                         <ul className="Col-List">
                             <li>Paul Hankins</li>
@@ -33,14 +61,14 @@ export default Footer;
                         </ul>
                     </div>
                     
-                    <div className="Column">
+                    <div className="col">
                                                <ul className="Col-List">
                             <li></li>
                             <li></li>
                             <li></li>
                         </ul>
                     </div>
-                    <div className="Column">
+                    <div className="col">
                         <h4>Some other dumbshit</h4>
                         <ul className="Col-List">
                             <li>Well this is beginning to look right atleast</li>
