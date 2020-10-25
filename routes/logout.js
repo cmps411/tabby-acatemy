d5ca7ce779dbfd93290bd70b295a4b48583c2bf8
piblo <paulhankins99@gmail.com>
2020-10-25 13:30:20 -0500

    logout fix?

--- routes/logout.js
+++ routes/logout.js
@@ -3,15 +3,9 @@ const passport = require("passport");
 require("../passport/passportConfig")(passport);
 
 router.get('/', (req, res) => {
-  try {
-    req.logout()
     req.session.destroy(function(err) {
-      res.send(err)
-    });
-  } catch(err) {
-    res.send(err)
-    console.log(err)
-  }
+      console.log(err)
+    })
   })
 
 module.exports = router
